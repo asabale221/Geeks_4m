d8cf03b1da1cdd7c7f1af1296d3b67d13e1dd5a3
asabale221 <119834965+asabale221@users.noreply.github.com>
2023-03-01 17:57:50 +0600

    homework_3

---
+++ posts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def hello(request):
+    # my_list = [1, 2, 3, 4]
+    body = "<h1>Hello</h1>"
+    # body = """
+    # <!DOCTYPE html>
+    #     <html>
+    #         <head>
+    #             <title>Geek TEST</title>
+    #         </head>
+    #         <body>
+    #
+    #             <h1>Заголовок первого уровня</h1>
+    #             <p>Параграф</p>
+    #
+    #         </body>
+    #     </html>
+    # """
+    headers = {"name": "Alex", }
+    # "Content-Type": "application/vnd.ms-excel",
+    # "Content-Disposition": "attachment; filename=file.xls"}
+    return HttpResponse(body, headers=headers, status=500)
+
+def get_index(request):
+    # print(request.user)
+    context = { 
+        "title": 'Main Page', 
+        "my_list": [1, 2, 3, 4], 
+    }
+    return render(request, 'posts/index.html', context=context)
+
+def get_about(request):
+    context = { 
+        "title": 'Страница о нас',
+    }
+    return render(request, "posts/about.html", context=context)
+
+def get_contacts(request):
+    context = { 
+        "title": 'Контакты',
+    }
+    return render(request, "posts/contact.html", context=context)
+    
